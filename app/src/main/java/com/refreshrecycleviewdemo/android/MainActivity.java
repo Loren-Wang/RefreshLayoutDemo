c5f9a9e8297e23ce27a0948f546ed59967f55f8e
wangliang <745342832@qq.com>
2017-06-19 11:34:09 +0800

    初始化刷新的recycleview的数据

--- app/src/main/java/com/refreshrecycleviewdemo/android/MainActivity.java
+++ app/src/main/java/com/refreshrecycleviewdemo/android/MainActivity.java
@@ -1,18 +1,62 @@
 package com.refreshrecycleviewdemo.android;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RefreshRecycleView test;
+    private Context context;
+    private LayoutInflater inflater;
+    private List<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
         test = (RefreshRecycleView) findViewById(R.id.test);
+        inflater = LayoutInflater.from(context);
 
-        
+        for(int i = 0 ; i < 20 ; i++){
+            list.add(String.valueOf(i));
+        }
+    }
+
+
+
+    private class RefreshRecycleViewAdapter extends RecyclerView.Adapter<Holder>{
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new Holder(inflater.inflate(R.layout.item_list_recycleview,null));
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            holder.textView.setText(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+    }
+
+    private class Holder extends RecyclerView.ViewHolder {
+        private TextView textView;
+        public Holder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.textView);
+        }
     }
 }
